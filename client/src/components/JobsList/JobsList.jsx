20bc4e9405696e0b74e40a14ed5911aba6c54597
Darius <dariuux@gmail.com>
2021-04-03 18:11:19 +0300

    Added pagination with button

--- client/src/components/JobsList/JobsList.jsx
+++ client/src/components/JobsList/JobsList.jsx
@@ -1,4 +1,4 @@
-import { CircularProgress, Container, Grid } from '@material-ui/core';
+import { Button, CircularProgress, Container, Grid } from '@material-ui/core';
 import React, { useEffect, useState } from 'react'
 import Job from './Job/Job';
 
@@ -7,22 +7,29 @@ const api = "https://jobs.github.com/positions.json?";
 const JobsList = () =>{
     const classes = useStyles()
     const [data,setData] = useState(null)
-
+    const [page, setpage] = useState(1)
     useEffect(() => {
         const fetchData = async () => {
-          const response = await fetch(`https://cors-anywhere.herokuapp.com/jobs.github.com/positions.json?`);
+          const response = await fetch(`https://cors-anywhere.herokuapp.com/jobs.github.com/positions.json?page=${page}`);
           const newData = await response.json();
           console.log(newData)
           setData(newData);
         };
         fetchData();
-      },[fetch]);
+      },[page]);
+     const handleClick = () => {
+          let count = page;
+          count++;
+          setpage(count)
+          console.log(page)
+      }
     return(
         <Container className={classes.container} size='lg'>
-            {data ? <Grid container spacing={5}>
+            {data ? <Grid className={classes.container} container spacing={5}>
                 {data.map(job => <Grid xl={12}> 
                     <Job company={job.company} title={job.title}/>
                 </Grid>)}
+                <Button className={classes.button} onClick={handleClick} variant="contained" color="primary">More jobs</Button>
             </Grid> : <CircularProgress className={classes.circular} size='8em'/>}
         </Container>
     )
--- client/src/components/JobsList/styles.js
+++ client/src/components/JobsList/styles.js
@@ -3,10 +3,20 @@ import { deepPurple } from '@material-ui/core/colors';
 
 export default makeStyles((theme) => ({
   container:{
-      marginTop: '4em'
+      marginTop: '4em',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
   },
   circular:{
     display: 'flex',
     textAlign: 'center'
+  },
+  button:{
+    marginBottom: '1em',
+    marginTop: '1em',
+    width: '100%',
+    color: 'black',
+    backgroundColor: 'white'
   }
 }));
